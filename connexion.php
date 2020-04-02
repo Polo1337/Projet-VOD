75c2c76724d6cd0e4b3465cfd48adb4c6f72d9a5
ikaran96 <59538431+ikaran96@users.noreply.github.com>
2020-04-03 00:18:14 +0200

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -13,31 +13,13 @@ require_once 'styleswitcher.php';
     <title>Connexion</title>
 
     <link rel="stylesheet" href="css/reset.css">
-<<<<<<< HEAD
-    
-    <link rel="stylesheet" media="screen, projection" type="text/css" id="css" href="<?php echo $url; ?>" />
-=======
-    <link rel="stylesheet" href="css/connexion.css">
-    <link rel="stylesheet" href="css/index.css">
->>>>>>> 002dd648dcecebfd252eabc5038b81305f5b35f5
+    <link rel="stylesheet" href="css/style_ilayda.css">
 
     <!--GOOGLE FONTS-->
 
     <link
         href="https://fonts.googleapis.com/css?family=Baloo+Tammudu+2:400,500,600,700,800|Ubuntu:300,300i,400,400i,500,500i,700,700i&display=swap"
         rel="stylesheet">
-<<<<<<< HEAD
-    <link
-        href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Rubik:300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
-        rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css?family=Asap:400,400i,500,500i,600,600i,700,700i|Bellota+Text:300,300i,400,400i,700,700i&display=swap"
-        rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Orbitron:700,800,900|Quicksand:300,400,500,600,700&display=swap"
-        rel="stylesheet">
-
-=======
->>>>>>> 002dd648dcecebfd252eabc5038b81305f5b35f5
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
@@ -60,15 +42,6 @@ include 'include/nav.php'; ?>
             <h2>Connexion</h2>
 
             <label><b>Nom d'utilisateur</b></label>
-<<<<<<< HEAD
-            <input class="login" type="text" placeholder="Nom d'utilisateur" name="username" required> <br>
-
-            <label><b>Email d'utilisateur</b></label>
-            <input class="login" type="text" placeholder="Mail d'utilisateur" name="mail_user" required> <br>
-
-            <label><b>Mot de passe</b></label>
-            <input class="login"  type="password" placeholder="Mot de passe" name="password_user" required><br>
-=======
             <input class="login" type="text" placeholder="Entrer le nom d'utilisateur" name="username" required> <br>
 
             <label><b>Email d'utilisateur</b></label>
@@ -76,7 +49,6 @@ include 'include/nav.php'; ?>
 
             <label><b>Mot de passe</b></label>
             <input class="login"  type="password" placeholder="Entrer le mot de passe" name="password_user" required><br>
->>>>>>> 002dd648dcecebfd252eabc5038b81305f5b35f5
 
             <input class="ok"type="submit" id='submit' value='LOGIN'> <br>
 
--- styleswitcher.php
+++ styleswitcher.php
@@ -6,17 +6,10 @@ function construire_url($dossier)
 }
 
 $dossiers = array(
-<<<<<<< HEAD
-	'index.css',
-	'pol/index2.css',
-	'steven/index3.css',
-  	'axel/index4.css'
-=======
 	'style.css',
 	'style2.css',
 	'style3.css',
   'style4.css'
->>>>>>> 002dd648dcecebfd252eabc5038b81305f5b35f5
 );
 
 $actuel = htmlspecialchars($_SERVER['PHP_SELF']);
